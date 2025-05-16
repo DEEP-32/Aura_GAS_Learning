c28da058d7fb029c0f83eda2a91af0f55b3bbca9
Deepanshu Singh <anshu3012ep@gmail.com>
2025-05-16 18:44:01 +0530

    [DEBUG] getting the tags of effect assets

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -5,11 +5,19 @@
 
 
 void UAuraAbilitySystemComponent::AbilityActorInfoSet() {
-	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this,&UAuraAbilitySystemComponent::EffectApplied);
+	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
 }
 
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AffectingComponent,
                                                 const FGameplayEffectSpec& Spec, FActiveGameplayEffectHandle Handle) {
-	GEngine->AddOnScreenDebugMessage(1, 5.0f, FColor::Blue, FString("Effect applied!"));
-}
+	FGameplayTagContainer TagContainer;
+	Spec.GetAllAssetTags(TagContainer);
 
+	for (const FGameplayTag& Tag : TagContainer) {
+		//TODO : Broadcast the tag to widget controller.
+
+		const FString TagString = FString::Printf(TEXT("Get Tag : %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1,5.0f,FColor::Blue,TagString);
+		
+	}
+}
