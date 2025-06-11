2123799348eb7ffdad7a2df9ca666b8a13bb7732
Deepanshu Singh <anshu3012ep@gmail.com>
2025-06-11 14:38:16 +0530

    [ADD] blueprint library for accessing widget controller

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,25 @@
+﻿// 
+
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "Player/AuraPlayerState.h"
+#include "UI/HUD/AuraHUD.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+
+UOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject) {
+
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject,0)) {
+		if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(PC->GetHUD())) {
+			AAuraPlayerState* PS = PC->GetPlayerState<AAuraPlayerState>();
+			UAbilitySystemComponent* AbilitySystemComponent = PS->GetAbilitySystemComponent();
+			UAttributeSet* AttributeSet = PS->GetAttributeSet();
+
+			const FWidgetControlledParams Params(PC,PS,AbilitySystemComponent,AttributeSet);
+			
+			return AuraHUD->GetOverlayWidgetController(Params);
+		}
+	}
+
+	return nullptr;
+}
