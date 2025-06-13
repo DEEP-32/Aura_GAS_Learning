e63cb9b9cfea6f0207a8db37c1aa54ab82eab9ed
Deepanshu Singh <anshu3012ep@gmail.com>
2025-06-13 14:14:53 +0530

    [ADD] gameplay tags with input actions

---
+++ Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -0,0 +1,20 @@
+﻿// 
+
+
+#include "Input/AuraInputConfig.h"
+
+const UInputAction* UAuraInputConfig::FindAbilityInputAction(const FGameplayTag& InputTag, bool bLogNotFound) const {
+
+	for (const FAuraInputAction& Action : AbilityInputActions) {
+		if (Action.InputAction && Action.InputTag == InputTag) {
+			return Action.InputAction;
+		}
+	}
+
+
+	if (bLogNotFound) {
+		UE_LOG(LogTemp, Error, TEXT("Input %s not found for ability input action"), *InputTag.ToString());
+	}
+
+	return nullptr;
+}
