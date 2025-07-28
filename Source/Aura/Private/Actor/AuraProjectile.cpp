640341b817fbe6db2464c4cc353d79fc068ae34a
Deepanshu Singh <anshu3012ep@gmail.com>
2025-07-28 13:11:48 +0530

    [ADD] impact sound and impact vfx

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -3,22 +3,25 @@
 
 #include "Actor/AuraProjectile.h"
 
+#include "NiagaraFunctionLibrary.h"
+#include "Components/AudioComponent.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 
 AAuraProjectile::AAuraProjectile() {
 	PrimaryActorTick.bCanEverTick = false;
 	bReplicates = true;
-	
+
 	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
 	SetRootComponent(Sphere);
 
 	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
-	Sphere->SetCollisionResponseToChannel(ECC_WorldDynamic,ECR_Overlap);
-	Sphere->SetCollisionResponseToChannel(ECC_WorldStatic,ECR_Overlap);
-	Sphere->SetCollisionResponseToChannel(ECC_Pawn,ECR_Overlap);
+	Sphere->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
+	Sphere->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovement");
 	ProjectileMovement->InitialSpeed = 550.f;
@@ -28,14 +31,56 @@ AAuraProjectile::AAuraProjectile() {
 
 void AAuraProjectile::BeginPlay() {
 	Super::BeginPlay();
-	Sphere->OnComponentBeginOverlap.AddDynamic(this,&ThisClass::AAuraProjectile::OnSphereOverlap);
-	
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::AAuraProjectile::OnSphereOverlap);
+	LoopingSoundComponent = UGameplayStatics::SpawnSoundAttached(
+		LoopingSound,
+		GetRootComponent()
+	);
+}
+
+void AAuraProjectile::Destroyed() {
+	if (!bHit && !HasAuthority()) {
+		LoopingSoundComponent->Stop();
+		UGameplayStatics::PlaySoundAtLocation(
+			this,
+			ImpactSound,
+			GetActorLocation(),
+			FRotator::ZeroRotator
+		);
+
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+			this,
+			ImpactEffect,
+			GetActorLocation()
+		);
+	}
+	Super::Destroyed();
 }
 
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+                                      UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                                      const FHitResult& SweepResult) {
+
+
+	LoopingSoundComponent->Stop();
 	
-	
+	UGameplayStatics::PlaySoundAtLocation(
+		this,
+		ImpactSound,
+		GetActorLocation(),
+		FRotator::ZeroRotator
+	);
+
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+		this,
+		ImpactEffect,
+		GetActorLocation()
+	);
+
+	if (HasAuthority()) {
+		Destroy();
+	}
+	else {
+		bHit = true;
+	}
 }
-
-
--- Source/Aura/Public/Actor/AuraProjectile.h
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -6,8 +6,11 @@
 #include "GameFramework/Actor.h"
 #include "AuraProjectile.generated.h"
 
+class UAudioComponent;
 class USphereComponent;
 class UProjectileMovementComponent;
+class UNiagaraSystem;
+
 
 UCLASS()
 class AURA_API AAuraProjectile : public AActor {
@@ -21,13 +24,28 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
-
+	virtual void Destroyed() override;
 	UFUNCTION(BlueprintCallable)
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 	
 
 private:
+
+	bool bHit = false;
+	
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USphereComponent> Sphere;
+
+	UPROPERTY(EditAnywhere,Category="Impact|VFX")
+	TObjectPtr<UNiagaraSystem> ImpactEffect;
+
+	UPROPERTY(EditAnywhere,Category="Impact|Audio")
+	TObjectPtr<USoundBase> ImpactSound;
+
+	UPROPERTY(EditAnywhere,Category="Impact|Audio")
+	TObjectPtr<USoundBase> LoopingSound;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UAudioComponent> LoopingSoundComponent;
 	
 };
