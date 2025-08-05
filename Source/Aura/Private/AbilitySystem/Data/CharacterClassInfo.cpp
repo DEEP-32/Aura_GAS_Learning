3bc4ba1988dc0cd464886fc20a19663e2c2ac449
Deepanshu Singh <anshu3012ep@gmail.com>
2025-08-05 23:02:33 +0530

    [ADD] character class info

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,7 @@
+﻿// 
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+FCharacterClassDefaultInfo UCharacterClassInfo::GetCharacterClassDefaultInfo(ECharacterClass CharacterClass) {
+	return CharacterClassInfo.FindChecked(CharacterClass);
+}
---
+++ Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,47 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+
+class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8 {
+	Elementalist,
+	Warrior,
+	Ranger
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo {
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Attribute")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Attribute")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Attribute")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+
+	
+	
+};
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UCharacterClassInfo : public UDataAsset {
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly)
+	TMap<ECharacterClass,FCharacterClassDefaultInfo> CharacterClassInfo;
+
+	FCharacterClassDefaultInfo GetCharacterClassDefaultInfo(ECharacterClass CharacterClass);
+};
